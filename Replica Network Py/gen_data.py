3320198084a6c333dfe19c0d27db2d52fc178fbb
Finnian Anderson <get@finnian.io>
2017-09-26 22:06:19 +0100

    Update gen_data.py

--- Replica Network Py/gen_data.py
+++ Replica Network Py/gen_data.py
@@ -1,7 +1,5 @@
 from PIL import Image
 import os
-from skimage import color
-import numpy as np
 import sys
 
 # Import settings
@@ -9,8 +7,6 @@ IM_WIDTH = 224
 IM_HEIGHT = 224
 DATA_DIR = 'resources'
 IM_PER_BIN = 100
-
-images_loaded = 0
 
 for filename in os.listdir(DATA_DIR):
     print(filename)
@@ -25,23 +21,3 @@ for filename in os.listdir(DATA_DIR):
     jpeg_cropped = jpeg_file.crop((x_cut, y_cut, jpeg_file.size[0] - x_cut, jpeg_file.size[1] - y_cut))
     jpeg_resized = jpeg_cropped.resize((IM_WIDTH, IM_HEIGHT))
     jpeg_resized.save(os.path.join(DATA_DIR, filename))
-    # Import RAW RGB data
-    raw = np.reshape(jpeg_resized.getdata(), (IM_WIDTH, IM_HEIGHT, 3)).astype(float)
-    # Convert RGB to LAB
-    lab = color.rgb2lab(raw / 256)
-    # Flatten LAB to Pixel Data
-    data = lab.flatten()
-
-    # print sys.getsizeof(data[0])
-
-    # Write image to binary file
-    file = open('bin/data' + str(images_loaded / IM_PER_BIN) + '.bin', 'ab')
-
-    for pix_col in data:
-        file.write(pix_col)
-
-    file.write(bytes(data))
-    file.close()
-
-    # Iterate images_loaded
-    images_loaded += 1
